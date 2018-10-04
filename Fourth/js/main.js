c097921c5788413ab6ffbed14d07ed98424151fc
Andrii <harasihataraku@yahoo.com>
2018-10-04 23:56:31 +0300

    3

--- Fourth/js/main.js
+++ Fourth/js/main.js
@@ -6,7 +6,7 @@
  /* var txt = document.getElementsByClassName('input-txt');
   var btn = document.getElementsByClassName('input-btn');
   var newlist = document.getElementsByClassName('newlists');*/
-  	addbtn.onclick = function(){ 
+  addbtn.onclick = function(){ 
 
     var mynewlist = document.createElement('li'); 
     mynewlist.innerHTML = todolist.value; 
@@ -14,21 +14,40 @@
     newlist.appendChild(mynewlist);    
     todolist.value = '';   
     var escbtn = document.createElement('button');
-  		 escbtn.innerHTML = "delete";
+  		escbtn.innerHTML = "delete";
    		escbtn.className = "deletebutton"; 
   		 newlist.appendChild(escbtn);      
-    };
-    	 /*escbtn.onclick = function(){
+       
+          escbtn.onclick = function(event){
+            var ullfordel = document.getElementById('ull');
+            var lifordel = ullfordel.getElementsByTagName('li'); 
+          var btnoff = document.getElementsByClassName('deletebutton');
+            var target = event.target;
+                 if (target.className != 'deletebutton') {
+                     return;
+                    btnoff.remove();
+                    lifordel.remove();
+                   };             
+         };
 
-          	var ullfordel = document.getElementById('ull');
-          	var lifordel = ullfordel.getElementsByTagName('li')[0]; 
-         	var btnoff = document.getElementsByClassName('deletebutton')[0];         
+  }; 
+          /* escbtn.onclick = function(){
+
+            var ullfordel = document.getElementById('ull');
+            var lifordel = ullfordel.getElementsByTagName('li')[0]; 
+          var btnoff = document.getElementsByClassName('deletebutton')[0];         
             btnoff.remove();
             lifordel.remove();
             console.log(lifordel);              
                
-         };  */
+         };*/ 
 }());
+
+
+
+
+
+
 /*for( var i = 0; i <= ullfordel.length - 1; i++) {
               var lifordel = document.getElementsByTagName('li');
                         
