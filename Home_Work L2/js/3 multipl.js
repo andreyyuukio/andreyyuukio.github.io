20c6fe1ff404606cfbe8d8aa86ce1566acb16abd
Andrii <harasihataraku@yahoo.com>
2018-10-22 19:00:31 +0300

    9

--- Home_Work L2/js/3 multipl.js
+++
@@ -1,9 +0,0 @@
-function homeWork3() {
-for(i = 1; i <= 10; i++) {
-    var a = 3 * i;       
- 		alert(a);
-  };	
-  
-};
- 
- 
--- Home_Work L2/js/5 to 1.js
+++
@@ -1,5 +0,0 @@
-function homeWork2() {
-	for(var n = 5; n >= 1; n--) {
-  		alert(n);
-	}; 	
-};
